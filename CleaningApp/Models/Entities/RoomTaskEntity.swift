b2e9fe50574076a0c312e9e896ca9ff17bace6a4
Jakub Kociucki <82371733+KociucKy@users.noreply.github.com>
2026-03-20 22:22:08 +0100

    Create SwiftData entities

---
+++ CleaningApp/Models/Entities/RoomTaskEntity.swift
@@ -0,0 +1,32 @@
+import Foundation
+import SwiftData
+
+@Model
+final class RoomTaskEntity {
+	// MARK: - Properties
+
+	var id: UUID
+	var name: String
+	var room: RoomEntity
+	var frequencyEncoded: String
+	var estimatedDuration: Int
+	var createdAt: Date
+
+	// MARK: - Init
+
+	init(
+		id: UUID = UUID(),
+		name: String,
+		room: RoomEntity,
+		frequencyEncoded: String,
+		estimatedDuration: Int,
+		createdAt: Date = Date()
+	) {
+		self.id = id
+		self.name = name
+		self.room = room
+		self.frequencyEncoded = frequencyEncoded
+		self.estimatedDuration = estimatedDuration
+		self.createdAt = createdAt
+	}
+}
