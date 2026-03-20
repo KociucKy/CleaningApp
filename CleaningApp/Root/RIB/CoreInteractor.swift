e58409ee4e5cb79d6aeafb23f1bc5b0a9de16739
Jakub Kociucki <82371733+KociucKy@users.noreply.github.com>
2026-03-20 23:50:36 +0100

    Add all managers methods to CoreInteractor

--- CleaningApp/Root/RIB/CoreInteractor.swift
+++ CleaningApp/Root/RIB/CoreInteractor.swift
@@ -6,11 +6,85 @@ import Foundation
 struct CoreInteractor {
 	// MARK: - Properties
 
-	private let container: DependencyContainer
+	private let roomManager: RoomManager
+	private let roomTaskManager: RoomTaskManager
+	private let completedTaskManager: CompletedTaskManager
+	private let skippedTaskManager: SkippedTaskManager
 
 	// MARK: - Init
 
 	init(container: DependencyContainer) {
-		self.container = container
+		self.roomManager = container.resolve(RoomManager.self)!
+		self.roomTaskManager = container.resolve(RoomTaskManager.self)!
+		self.completedTaskManager = container.resolve(CompletedTaskManager.self)!
+		self.skippedTaskManager = container.resolve(SkippedTaskManager.self)!
+	}
+
+	// MARK: - Room Manager
+
+	func fetchAllRooms() throws -> [Room] {
+		try roomManager.fetchAll()
+	}
+
+	func saveRoom(_ item: Room) throws {
+		try roomManager.save(item)
+	}
+
+	func deleteRoom(_ item: Room) throws {
+		try roomManager.delete(item)
+	}
+
+	// MARK: - Room Task Manager
+
+	func fetchAllRoomTasks() throws -> [RoomTask] {
+		try roomTaskManager.fetchAll()
+	}
+
+	func fetchAllRoomTasks(for roomId: UUID) throws -> [RoomTask] {
+		try roomTaskManager.fetchAll(for: roomId)
+	}
+
+	func saveRoomTask(_ item: RoomTask) throws {
+		try roomTaskManager.save(item)
+	}
+
+	func deleteRoomTask(_ item: RoomTask) throws {
+		try roomTaskManager.delete(item)
+	}
+
+	// MARK: - Completed Task Manager
+
+	func fetchAllCompletedTasks() throws -> [CompletedTask] {
+		try completedTaskManager.fetchAll()
+	}
+
+	func fetchAllCompletedTasks(for taskId: UUID) throws -> [CompletedTask] {
+		try completedTaskManager.fetchAll(for: taskId)
+	}
+
+	func saveCompletedTask(_ item: CompletedTask) throws {
+		try completedTaskManager.save(item)
+	}
+
+	func deleteCompletedTask(_ item: CompletedTask) throws {
+		try completedTaskManager.delete(item)
+	}
+
+	// MARK: - Skipped Task Manager
+
+	func fetchAllSkippedTasks() throws -> [SkippedTask] {
+		try skippedTaskManager.fetchAll()
+	}
+
+	func fetchAllSkippedTasks(for taskId: UUID) throws -> [SkippedTask] {
+		try skippedTaskManager.fetchAll(for: taskId)
+	}
+
+	func saveSkippedTask(_ item: SkippedTask) throws {
+		try skippedTaskManager.save(item)
+	}
+
+	func deleteSkippedTask(_ item: SkippedTask) throws {
+		try skippedTaskManager.delete(item)
 	}
 }
