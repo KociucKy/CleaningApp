8e627214a1a0aabc049458f9bf5f232d2d66ac44
Jakub Kociucki <82371733+KociucKy@users.noreply.github.com>
2026-04-02 15:50:24 +0200

    Add displayName property to Frequency

--- CleaningApp/Models/Domain/Frequency.swift
+++ CleaningApp/Models/Domain/Frequency.swift
@@ -13,4 +13,23 @@ enum Frequency: Equatable, Codable {
 	case quarterly
 	case biannually
 	case yearly
+
+	// MARK: - Display
+
+	var displayName: String {
+		switch self {
+		case .daily: "Every day"
+		case .everyOtherDay: "Every other day"
+		case let .everyXDays(days): "Every \(days) days"
+		case let .timesPerWeek(times): times == 1 ? "Once a week" : "\(times)× per week"
+		case .everyOtherWeek: "Every other week"
+		case let .everyXWeeks(weeks): "Every \(weeks) weeks"
+		case let .timesPerMonth(times): times == 1 ? "Once a month" : "\(times)× per month"
+		case .monthly: "Once a month"
+		case let .everyXMonths(months): "Every \(months) months"
+		case .quarterly: "Every 3 months"
+		case .biannually: "Twice a year"
+		case .yearly: "Once a year"
+		}
+	}
 }
