77d89437e67137a4a288547a9c9df4e05e78e1e6
Jakub Kociucki <82371733+KociucKy@users.noreply.github.com>
2026-09-09 17:32:09 +0200

    [CA-3] - Add router methods for displaying system errors and alerts

--- CleaningApp/Root/RIB/CoreRouter.swift
+++ CleaningApp/Root/RIB/CoreRouter.swift
@@ -10,7 +10,7 @@ struct CoreRouter {
 	let router: Router
 	let builder: CoreBuilder
 
-	// MARK: - Navigation
+	// MARK: - Core Navigation
 
 	func dismissScreen() {
 		router.dismissScreen()
@@ -30,6 +30,24 @@ struct CoreRouter {
 
 	func dismissAlert() {
 		router.dismissAlert()
+	}
+
+	func showAlert(_ option: AlertType, title: String, subtitle: String?, buttons: (@Sendable () -> AnyView)?){
+		router.showAlert(
+			option,
+			title: title,
+			subtitle: subtitle,
+			buttons: buttons
+		)
+	}
+
+	func showAlert(error: Error) {
+		router.showAlert(
+			.alert,
+			title: "Error",
+			subtitle: error.localizedDescription,
+			buttons: nil
+		)
 	}
 
 	// MARK: - Dev Settings
