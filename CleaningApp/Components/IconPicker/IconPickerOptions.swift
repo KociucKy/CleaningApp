31dd26790aacf9514e19e0cad37e6f35469e5ed8
Jakub Kociucki <82371733+KociucKy@users.noreply.github.com>
2026-09-15 14:18:52 +0200

    [CA-16] - Extract icon picker options to separate enum

---
+++ CleaningApp/Components/IconPicker/IconPickerOptions.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+// MARK: - IconPickerOptions
+
+enum IconPickerOptions {
+    static let icons: [String] = [
+        "fork.knife",
+        "sofa",
+        "bed.double",
+        "shower",
+        "door.left.hand.open",
+        "desktopcomputer",
+        "car",
+        "washer",
+        "toilet",
+        "fork.knife.circle",
+        "house.fill",
+        "bed.double.fill",
+        "dumbbell",
+        "book.fill",
+        "paintpalette.fill",
+        "leaf.fill",
+        "wrench.and.screwdriver.fill",
+        "music.note",
+        "gamecontroller.fill",
+        "laptopcomputer",
+        "tv.fill",
+        "car.fill",
+        "cart.fill",
+        "tent.fill",
+        "pawprint.fill",
+        "figure.walk",
+        "tshirt.fill",
+        "cup.and.saucer.fill",
+        "square.grid.2x2",
+    ]
+}
