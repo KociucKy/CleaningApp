c27847933774bfbde8b29aec4620740cca017c20
Jakub Kociucki <82371733+KociucKy@users.noreply.github.com>
2026-09-15 14:19:36 +0200

    [CA-16] - Update IconPickerView and it's presenter

--- CleaningApp/Components/IconPicker/IconPickerPresenter.swift
+++ CleaningApp/Components/IconPicker/IconPickerPresenter.swift
@@ -13,37 +13,7 @@ final class IconPickerPresenter {
 	private let room: Room?
 	private let onRoomSaved: ((Room) -> Void)?
 
-	let icons: [String] = [
-		"fork.knife",
-		"sofa",
-		"bed.double",
-		"shower",
-		"door.left.hand.open",
-		"desktopcomputer",
-		"car",
-		"washer",
-		"toilet",
-		"fork.knife.circle",
-		"house.fill",
-		"bed.double.fill",
-		"dumbbell",
-		"book.fill",
-		"paintpalette.fill",
-		"leaf.fill",
-		"wrench.and.screwdriver.fill",
-		"music.note",
-		"gamecontroller.fill",
-		"laptopcomputer",
-		"tv.fill",
-		"car.fill",
-		"cart.fill",
-		"tent.fill",
-		"pawprint.fill",
-		"figure.walk",
-		"tshirt.fill",
-		"cup.and.saucer.fill",
-		"square.grid.2x2",
-	]
+	let icons = IconPickerOptions.icons
 
 	var selectedIcon: String
 
