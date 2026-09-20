a28a478c6704dc458a528bc6fe533692293cde4f
Jakub Kociucki <82371733+KociucKy@users.noreply.github.com>
2026-09-20 19:42:36 +0200

    [CA-23] - Fill in presenter properties

--- CleaningApp/Core/ActivityLog/Presentation/ActivityLogPresenter.swift
+++ CleaningApp/Core/ActivityLog/Presentation/ActivityLogPresenter.swift
@@ -3,5 +3,18 @@ import Foundation
 @Observable
 @MainActor
 final class ActivityLogPresenter {
-	
+	// MARK: - Properties
+
+	private let interactor: any ActivityLogInteractor
+	private let router: any ActivityLogRouter
+
+	// MARK: - Init
+
+	init(
+		interactor: any ActivityLogInteractor,
+		router: any ActivityLogRouter
+	) {
+		self.interactor = interactor
+		self.router = router
+	}
 }
